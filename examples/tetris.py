04e0cf5fdec3a100e7f17fad03c216c4d841295a
Bogdan Dobrica <bdobrica@gmail.com>
2023-05-14 21:13:58 +0300

    Update tetris.py

--- examples/tetris.py
+++ examples/tetris.py
@@ -56,17 +56,6 @@ def init_matrix() -> RGBMatrix:
     options.cols = WIDTH
     options.chain_length = 1
     options.parallel = 1
-    options.row_address_type = 0
-    options.multiplexing = 0
-    options.pwm_bits = 11
-    options.brightness = 100
-    options.pwm_lsb_nanoseconds = 130
-    options.led_rgb_sequence = "RGB"
-    options.pixel_mapper_config = ""
-    options.panel_type = ""
-    options.gpio_slowdown = 1
-    options.disable_hardware_pulsing = False
-    options.drop_privileges = False
 
     matrix = RGBMatrix(options=options)
     matrix.Clear()
